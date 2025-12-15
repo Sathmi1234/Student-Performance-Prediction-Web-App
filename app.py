0725fcd8dcc66b0eea6906494fb8c12839e25756
Sathmi1234 <devsadini@gmail.com>
2025-12-15 11:09:46 +0530

    feat: creating the UI

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ import numpy as np
 
 # Load trained model
 model = joblib.load("model.pkl")
+
+st.title("🎓 Student Performance Prediction")
+st.write("Based on Portuguese student dataset")
+
+# User inputs
+studytime = st.slider("Study Time (1–4)", 1, 4, 2)
+failures = st.slider("Past Class Failures", 0, 4, 0)
+absences = st.number_input("Number of Absences", 0, 100, 5)
+g1 = st.slider("First Period Grade (0–20)", 0, 20, 10)
+g2 = st.slider("Second Period Grade (0–20)", 0, 20, 10)
